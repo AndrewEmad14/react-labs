d7958eee98d6eaf5087959fd9c7bdd139368479f
Andrew Emad <andrew14emad@gmail.com>
2026-04-02 16:27:20 +0200

    adjustments

--- lab2/user-project/src/components/search-bar.jsx
+++ lab2/user-project/src/components/search-bar.jsx
@@ -3,17 +3,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useState } from "react"
 const SearchBar = ({handleQuery})=>{
     const MAX_LENGTH=100
-
+  
+    const [query,setQuery] = useState('');
     const handleSubmit = (e)=>{
+        setQuery(e.target.value);
         handleQuery(e.target.value)
+    }
+    const handleReset = ()=>{
+        setQuery('');
+        handleQuery('')
     }
     return(
         <div className="flex flex-row w-full px-50 py-20">
             <input className="rounded flex-1 border border-orange-500 
-            focus:outline-none focus:border-yellow-300" type="search" maxLength={MAX_LENGTH} onChange={handleSubmit} />
-            <button type="submit" onClick={handleSubmit}>
-                <FontAwesomeIcon className="bg-orange-400 text-white p-5  mx-5 rounded-lg" icon={faSearch}/>
-            </button>
+            focus:outline-none focus:border-yellow-300" type="search" value={query} maxLength={MAX_LENGTH} onChange={handleSubmit} />
+
+            {query?<button onClick={handleReset} className="p-5 mx-5 rounded-lg bg-red-500">reset</button> :<FontAwesomeIcon className="bg-orange-400 text-white p-5  mx-5 rounded-lg" icon={faSearch}/>}    
 
 
         </div>
